78cef0552865e81144423bb24483e5628552c268
dillonnollid <dillonnollid@gmail.com>
2019-11-11 18:46:59 +0000

    Added a spinner object and some text to homescreen

--- app/src/main/java/com/example/orderyurt/MainActivity.java
+++ app/src/main/java/com/example/orderyurt/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.orderyurt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = new Spinner(this);
+        String restaurantArray[] = {"Lana", "McDonalds", "Scabies"};
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item,
+                        restaurantArray); //selected item will look like a spinner set from XML
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout
+                .simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerArrayAdapter);
     }
 }
