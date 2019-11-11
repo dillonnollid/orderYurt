2718b577d1afcfeecda94fbc0627e93e3d72c69e
Brian <16190319@studentmail.ul.ie>
2019-11-11 20:13:35 +0000

    added item and menu

---
+++ app/src/main/java/com/example/orderyurt/Item.java
@@ -0,0 +1,8 @@
+package com.example.orderyurt;
+
+public class Item {
+    private String itemID;
+    private String itemName;
+    private String itemDescription;
+    private float itemPrice;
+}
---
+++ app/src/main/java/com/example/orderyurt/Menu.java
@@ -0,0 +1,17 @@
+package com.example.orderyurt;
+
+public class Menu {
+    private String menuID;
+    private String menuName;
+    private String menuDesc;
+    private Item[] items;
+
+    public void addItem( String itemID ){
+
+    }
+
+    public Item[] getItems(){
+        return items;
+    }
+}
+
