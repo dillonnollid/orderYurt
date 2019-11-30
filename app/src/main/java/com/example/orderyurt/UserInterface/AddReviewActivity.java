463ee22d74e979da63c109bf2de15e1032b9c501
AdamsDomain <16160037@studentmail.ul.ie>
2019-11-30 23:54:29 +0000

    Adding Create Review Functionality.

--- app/src/main/java/com/example/orderyurt/UserInterface/AddReviewActivity.java
+++ app/src/main/java/com/example/orderyurt/UserInterface/AddReviewActivity.java
@@ -1,11 +1,55 @@
 package com.example.orderyurt.UserInterface;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.orderyurt.Accounts.RestaurantUser;
+import com.example.orderyurt.Discount.Coupon;
+import com.example.orderyurt.Review.Review;
+
 public class AddReviewActivity extends AppCompatActivity {
-    //Text box for name, description
-    //Dropdown for stars?
-    //button to post
-    //button to exit
-    //Add review to DB with userID and restaurantID
+    //Create a Review object and add it to DB for this restaurant
+    RestaurantUser rUser = new RestaurantUser();
+    Review newReview;
+    Button addReviewButton;
+    EditText title, description;
+    String reviewTitle, reviewDescription;
+    RatingBar ratingBar;
+    float rating;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+
+        addReviewButton = (Button) findViewById(R.id.addReviewButton);
+        title = (EditText) findViewById(R.id.reviewTitle);
+        description = (EditText) findViewById(R.id.reviewDescription);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+        reviewTitle = title.getText().toString();
+        reviewDescription = description.getText().toString();
+        rating = ratingBar.getRating();
+
+        addReviewButton.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        newReview = new Review(reviewTitle, reviewDescription, rating);
+                        rUser.addReview(newReview);
+                        Toast.makeText(AddReviewActivity.this, R.string.review_added,
+                                Toast.LENGTH_LONG).show();
+
+                        Intent myIntent = new Intent(AddReviewActivity.this, RestaurantPageActivity.class);
+                        AddReviewActivity.this.startActivity(myIntent);
+                    }
+                });
+
+    }
 }
