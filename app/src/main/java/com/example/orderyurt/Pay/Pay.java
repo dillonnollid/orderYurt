34a2090de75d9c00550a0894efbe9bf680b9981d
dillonnollid <dillonnollid@gmail.com>
2019-11-30 21:12:16 +0000

    Added more functionality to Decorator design pattern, although the verifyCard method isn't working, causes program to crash. Will return and finish it

--- app/src/main/java/com/example/orderyurt/Pay/Pay.java
+++ app/src/main/java/com/example/orderyurt/Pay/Pay.java
@@ -1,8 +1,12 @@
 package com.example.orderyurt.Pay;
 
 public interface Pay {
-    boolean verifyCard(float cardNum, int csv);
+    boolean verifyCard(String cardNum, String cvv);
+    boolean isCardValid();
     boolean verifyPayment(int amount);
     String getPaymentInfo();
+    void setPaymentInfo(String cardNum, String cvv);
+    boolean result=false;
+
 
 }
--- app/src/main/java/com/example/orderyurt/Pay/PayDecorator.java
+++ app/src/main/java/com/example/orderyurt/Pay/PayDecorator.java
@@ -1,14 +1,27 @@
 package com.example.orderyurt.Pay;
 
 public abstract class PayDecorator implements Pay {
-    private final Pay decoratedPay;
+    private final BasePay decoratedPay;
 
 
-    public PayDecorator(Pay pay) {
+    public PayDecorator(BasePay pay) {
         this.decoratedPay = pay;
     }
 
     public String getPaymentInfo(){
         return decoratedPay.getPaymentInfo();
+    }
+
+    public void setPaymentInfo(String cardNum, String cvv){
+        decoratedPay.setPaymentInfo(cardNum, cvv);
+    }
+    public boolean verifyCard(String cardNum, String cvv){
+        return decoratedPay.verifyCard(cardNum, cvv);
+    }
+    public boolean verifyPayment(int amount){
+        return decoratedPay.verifyPayment(amount);
+    }
+    public boolean isCardValid(){
+        return decoratedPay.isCardValid();
     }
 }
