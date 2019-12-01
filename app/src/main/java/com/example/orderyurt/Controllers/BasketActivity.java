35b2a6fc226726bb9f210760e8806447f158bc21
dillonnollid <dillonnollid@gmail.com>
2019-12-01 19:18:48 +0000

    Improved Order Functionality. Ready to talk with basket now

--- app/src/main/java/com/example/orderyurt/Controllers/BasketActivity.java
+++ app/src/main/java/com/example/orderyurt/Controllers/BasketActivity.java
@@ -2,8 +2,12 @@ package com.example.orderyurt.Controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,8 +23,35 @@ public class BasketActivity extends AppCompatActivity {
         setContentView(R.layout.activity_basket);
         Intent i = getIntent();
         Bundle b = i.getExtras();
-        ListView basketList = (ListView) findViewById(R.id.basketList);
+        String [] items = b.getStringArray("items");
+        String line="";
+        final String rest = b.getString("restName"), temp;
+        final double price = i.getDoubleExtra("totalprice", 0.0);
+        //ListView basketList = (ListView) findViewById(R.id.basketList);
         TextView totalCost = (TextView) findViewById(R.id.totalCostText);
+        totalCost.setText("Your order from " + rest + "\nTotal price of basket is " + String.valueOf(price));
+        TextView basketContents = findViewById(R.id.basketContents);
 
+        for(int j=0; j < items.length; j++){
+            if(items[j] != null) {
+                line += items[j] + "\n";
+            }
+        }
+        basketContents.setText(line);
+        temp = line;
+
+
+        Button order = (Button) findViewById(R.id.payButton);
+        order.setText("Order Now");
+        order.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        Intent myIntent = new Intent(BasketActivity.this, OrderActivity.class);
+                        myIntent.putExtra("restName", rest); //Optional parameters
+                        myIntent.putExtra("orderList", temp);
+                        myIntent.putExtra("totalprice", price);
+                        BasketActivity.this.startActivity(myIntent);
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/orderyurt/Controllers/OrderActivity.java
+++ app/src/main/java/com/example/orderyurt/Controllers/OrderActivity.java
@@ -2,9 +2,11 @@ package com.example.orderyurt.Controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.orderyurt.Controllers.R;
 import com.example.orderyurt.Menu.Item;
@@ -19,22 +21,15 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-        Basket b = new Basket(1);
-        Order temp = new Order(b);
-        ArrayList<Item> items = new ArrayList<>();//temp.getItems();
-        items.add(new Item("Pasta", "Boom", 11.50));
-        items.add(new Item("Pastazz", "Boom", 11.50));
-        items.add(new Item("Pastazzzz", "Boom", 11.50));
-        ArrayList<String> names = new ArrayList<>();//temp.getItems();
-        for(int i=0; i < items.size(); i++){
-            names.add(items.get(i).getName());
-        }
-        names.add("Wings");
-        names.add("Burgers");
-        //items.add(new Item("Pasta", "Boom", 11.50));
-        ArrayAdapter mPairedDevicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
-        ListView orderContents = (ListView) findViewById(R.id.basketList);
-        orderContents.setAdapter(mPairedDevicesAdapter);
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        String items = b.getString("orderList");
+        Double price = b.getDouble("totalprice");
+        
+        TextView contents = findViewById(R.id.basketList);
+        TextView basketPrice = findViewById(R.id.basketPrice);
+        basketPrice.setText("Total cost of basket is " + String.valueOf(price));
+        contents.setText(items);
 
     }
 }
